0b3f68d0098b8d2fd3685bbb0ededf5e7891d9e0
nrasel <nrasel452@gmail.com>
2024-01-12 13:20:21 +0600

    billing system

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,7 +1,1 @@
-  // while (continueTransaction != 0)
-    // {
-    //     printf("\n\t===========*Available Transaction*===========");
-    //     printf("\n\n\t1.Withdrawl");
-    //     printf("\n\n\t2.Deposit");
-    //     printf("\n\n\t3.Check Balance");
-    // }
+name
